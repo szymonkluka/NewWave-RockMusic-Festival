035e2c225b03a0930a3a42afcc58afdd91a59f17
szymonkluka <110814419+szymonkluka@users.noreply.github.com>
2023-08-21 14:33:18 +0200

    Update Footer.js

--- client/src/components/layout/Footer/Footer.js
+++ client/src/components/layout/Footer/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => (
   <footer id="sticky-footer" className="py-4 mt-5 bg-dark text-white-50">
     <div className="text-center">
-      <small>Copyright &copy; New Wave Festival 2019</small>
+      <small>Copyright &copy; New Wave Festival 2023</small>
     </div>
   </footer>
 );
